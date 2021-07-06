83136ec0c82075882e30d1c300cf5c44dd26a58c
Kobe <kobe@lenjou.be>
2021-07-06 21:01:40 +0200

    First Blazor spike

--- src/PP.Blazor/Pages/Index.razor.cs
+++ src/PP.Blazor/Pages/Index.razor.cs
@@ -1,7 +1,18 @@
-﻿namespace PP.Blazor.Pages
+﻿using Microsoft.AspNetCore.Components;
+using PP.Attractions;
+using System.Threading.Tasks;
+
+namespace PP.Blazor.Pages
 {
     public partial class Index
     {
+        [Inject]
+        private IAttractionAppService AttractionAppService { get; set; }
+        private AttractionDto attraction = new AttractionDto();
 
+        protected async override Task OnInitializedAsync()
+        {
+            attraction = await AttractionAppService.GetOne(System.Guid.Parse("2EA5C79F-C268-8BC0-A785-39FB355C16B6"));
+        }
     }
 }
