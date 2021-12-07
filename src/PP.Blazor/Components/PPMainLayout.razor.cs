d792caf6006e51cc04e5b65625582b989e6b4e95
smartens21 <svenmartens21@gmail.com>
2021-12-07 21:05:37 +0100

    Upgrade from V4.3.3 to V4.4 Remove Blazor

--- src/PP.Blazor/Components/PPMainLayout.razor.cs
+++ src/PP.Blazor/Components/PPMainLayout.razor.cs
@@ -1,27 +1,23 @@
 ﻿using System;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Routing;
-using Volo.Abp.UI.Navigation;
 
 namespace Volo.Abp.AspNetCore.Components.Web.BasicTheme.Themes.Basic
 {
-    public partial class FirstLevelNavMenuItem : IDisposable
+    public partial class PPMainLayout : IDisposable
     {
         [Inject] private NavigationManager NavigationManager { get; set; }
 
-        [Parameter]
-        public ApplicationMenuItem MenuItem { get; set; }
-
-        public bool IsSubMenuOpen { get; set; }
+        private bool IsCollapseShown { get; set; }
 
         protected override void OnInitialized()
         {
             NavigationManager.LocationChanged += OnLocationChanged;
         }
 
-        private void ToggleSubMenu()
+        private void ToggleCollapse()
         {
-            IsSubMenuOpen = !IsSubMenuOpen;
+            IsCollapseShown = !IsCollapseShown;
         }
 
         public void Dispose()
@@ -31,7 +27,7 @@ namespace Volo.Abp.AspNetCore.Components.Web.BasicTheme.Themes.Basic
 
         private void OnLocationChanged(object sender, LocationChangedEventArgs e)
         {
-            IsSubMenuOpen = false;
+            IsCollapseShown = false;
             InvokeAsync(StateHasChanged);
         }
     }
